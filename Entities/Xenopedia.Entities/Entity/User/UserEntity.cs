808560a39b0bb9511c66cebf0d32dc7f59ff0d29
Alexandru Ban <alexandru.ban.drmer@gmail.com>
2023-02-09 20:37:08 +0100

    Add User Entity

---
+++ Entities/Xenopedia.Entities/Entity/User/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Xenopedia.Entities.Entity.User
+{
+    public class UserEntity
+    {
+        public int IdUser { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public DateTime? CreationDate { get; set; } = DateTime.Now;
+    }
+}
